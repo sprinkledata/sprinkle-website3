91110fef8ef34fffdbcb37cf3a1d47fc0301de25
sprinkle-sanchay <sanchay.sethi@sprinkledata.com>
2026-03-31 15:06:31 +0530

    Front page First lookup

---
+++ netlify/functions/get-web3forms-key.js
@@ -0,0 +1,12 @@
+exports.handler = async (event, context) => {
+  return {
+    statusCode: 200,
+    headers: {
+      "Content-Type": "application/json",
+      "Access-Control-Allow-Origin": "*",
+    },
+    body: JSON.stringify({
+      accessKey: process.env.WEB3FORMS_ACCESS_KEY
+    }),
+  };
+};
